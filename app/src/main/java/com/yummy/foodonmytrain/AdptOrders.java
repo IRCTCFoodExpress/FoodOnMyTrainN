46d9fd1ee60c98f079d30e1fb94ac65fd3c60a84
smm850740 <saurabh.malpure@tatatechnologies.com>
2019-12-31 01:58:36 +0530

    -adpt updated

--- app/src/main/java/com/yummy/foodonmytrain/AdptOrders.java
+++ app/src/main/java/com/yummy/foodonmytrain/AdptOrders.java
@@ -5,12 +5,17 @@ import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
 
@@ -28,15 +33,27 @@ public class AdptOrders extends RecyclerView.Adapter<AdptOrders.CustHolder> {
     @Override
     public CustHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        return new CustHolder(LayoutInflater.from(context).inflate(R.layout.lay_adpt_orders_list, parent, false));
+        return new CustHolder(LayoutInflater.from(context).inflate(R.layout.adapter_orderlist, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull CustHolder holder, int position) {
         Order order= orderArrayListArrayList.get(position);
-        holder.cust_name.setText(order.customername);
-        holder.cust_phone.setText(order.customer_phone);
-        holder.cust_seat.setText(order.customer_seatno);
+        holder.edtAdptOrderListName.setText(order.customername);
+        holder.edtAdptOrderListNumber.setText(order.customer_phone);
+        holder.edtAdptOrderListSeatNumber.setText(order.customer_seatno);
+
+
+        holder.txtAdptOrderListTeaCoffeeCount.setText(String.valueOf(order.tea_coffee));
+        holder.txtAdptOrderListTeaCoffeeCount.setText(String.valueOf(order.breakfast_veg));
+        holder.txtAdptOrderListTeaCoffeeCount.setText(String.valueOf(order.breakfast_nonveg));
+        holder.txtAdptOrderListTeaCoffeeCount.setText(String.valueOf(order.meal_veg));
+        holder.txtAdptOrderListTeaCoffeeCount.setText(String.valueOf(order.meal_egg));
+        holder.txtAdptOrderListTeaCoffeeCount.setText(String.valueOf(order.meal_chicken));
+        holder.txtAdptOrderListTeaCoffeeCount.setText(String.valueOf(order.biryani_veg));
+        holder.txtAdptOrderListTeaCoffeeCount.setText(String.valueOf(order.biryani_egg));
+        holder.txtAdptOrderListTeaCoffeeCount.setText(String.valueOf(order.biryani_chicken));
+
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -98,17 +115,65 @@ public class AdptOrders extends RecyclerView.Adapter<AdptOrders.CustHolder> {
     }
 
     class CustHolder extends RecyclerView.ViewHolder {
-        TextView cust_name;
-        TextView cust_phone;
-        TextView cust_seat;
-        CardView cardViewContact;
+
+        EditText edtAdptOrderListName,edtAdptOrderListNumber,edtAdptOrderListTrainNumber,edtAdptOrderListSeatNumber;
+        TextView txtAdptOrderListTeaCoffeeCount,txtAdptOrderListTeaCoffeePrice,
+                txtAdptOrderListBreakfastCount,txtAdptOrderListBreakfastPrice,
+                txtAdptOrderListBreakfastNonCount,txtAdptOrderListBreakfastNonPrice,
+                txtAdptOrderListMealCount,txtAdptOrderListMealPrice,
+                txtAdptOrderListMealEggCount,txtAdptOrderListMealEggPrice,
+                txtAdptOrderListMealChickenCount,txtAdptOrderListMealChickenPrice,
+                txtAdptOrderListBiryaniCount,txtAdptOrderListBiryaniPrice,
+                txtAdptOrderListBiryaniEggCount,txtAdptOrderListBiryaniEggPrice,
+                txtAdptOrderListTotal,txtAdptOrderListStatus;
+        Button btnAdptOrderListStatus;
+        TextInputLayout txtlayAdptOrderListNameTitle,txtlayAdptOrderListNumberTitle,
+                txtlayAdptOrderListTrainIdTitle,txtlayAdptOrderListSeatNoTitle;
+        LinearLayout layAdptOrderListItem1,layAdptOrderListItem2,layAdptOrderListItem3,
+                layAdptOrderListItem4,layAdptOrderListItem5,layAdptOrderListItem6,
+                layAdptOrderListItem7,layAdptOrderListItem8;
 
         CustHolder(View itemView) {
             super(itemView);
-            cust_name = itemView.findViewById(R.id.cust_name);
-            cust_phone = itemView.findViewById(R.id.cust_phone);
-            cust_seat = itemView.findViewById(R.id.cust_seat);
-            cardViewContact = itemView.findViewById(R.id.cardView);
+            edtAdptOrderListName=itemView.findViewById(R.id.edtAdptOrderListName);
+            edtAdptOrderListNumber=itemView.findViewById(R.id.edtAdptOrderListNumber);
+            edtAdptOrderListTrainNumber=itemView.findViewById(R.id.edtAdptOrderListTrainNumber);
+            edtAdptOrderListSeatNumber=itemView.findViewById(R.id.edtAdptOrderListSeatNumber);
+
+            txtAdptOrderListTeaCoffeeCount=itemView.findViewById(R.id.txtAdptOrderListTeaCoffeeCount);
+            txtAdptOrderListTeaCoffeePrice=itemView.findViewById(R.id.txtAdptOrderListTeaCoffeePrice);
+            txtAdptOrderListBreakfastCount=itemView.findViewById(R.id.txtAdptOrderListBreakfastCount);
+            txtAdptOrderListBreakfastPrice=itemView.findViewById(R.id.txtAdptOrderListBreakfastPrice);
+            txtAdptOrderListBreakfastNonCount=itemView.findViewById(R.id.txtAdptOrderListBreakfastNonCount);
+            txtAdptOrderListBreakfastNonPrice=itemView.findViewById(R.id.txtAdptOrderListBreakfastNonPrice);
+            txtAdptOrderListMealCount=itemView.findViewById(R.id.txtAdptOrderListMealCount);
+            txtAdptOrderListMealPrice=itemView.findViewById(R.id.txtAdptOrderListMealPrice);
+            txtAdptOrderListMealEggCount=itemView.findViewById(R.id.txtAdptOrderListMealEggCount);
+            txtAdptOrderListMealEggPrice=itemView.findViewById(R.id.txtAdptOrderListMealEggPrice);
+            txtAdptOrderListMealChickenCount=itemView.findViewById(R.id.txtAdptOrderListMealChickenCount);
+            txtAdptOrderListMealChickenPrice=itemView.findViewById(R.id.txtAdptOrderListMealChickenPrice);
+            txtAdptOrderListBiryaniCount=itemView.findViewById(R.id.txtAdptOrderListBiryaniCount);
+            txtAdptOrderListBiryaniPrice=itemView.findViewById(R.id.txtAdptOrderListBiryaniPrice);
+            txtAdptOrderListBiryaniEggCount=itemView.findViewById(R.id.txtAdptOrderListBiryaniEggCount);
+            txtAdptOrderListBiryaniEggPrice=itemView.findViewById(R.id.txtAdptOrderListBiryaniEggPrice);
+            txtAdptOrderListTotal=itemView.findViewById(R.id.txtAdptOrderListTotal);
+            txtAdptOrderListStatus=itemView.findViewById(R.id.txtAdptOrderListStatus);
+
+            btnAdptOrderListStatus=itemView.findViewById(R.id.btnAdptOrderListStatus);
+
+            txtlayAdptOrderListNameTitle=itemView.findViewById(R.id.txtlayAdptOrderListNameTitle);
+            txtlayAdptOrderListNumberTitle=itemView.findViewById(R.id.txtlayAdptOrderListNumberTitle);
+            txtlayAdptOrderListTrainIdTitle=itemView.findViewById(R.id.txtlayAdptOrderListTrainIdTitle);
+            txtlayAdptOrderListSeatNoTitle=itemView.findViewById(R.id.txtlayAdptOrderListSeatNoTitle);
+
+            layAdptOrderListItem1=itemView.findViewById(R.id.layAdptOrderListItem1);
+            layAdptOrderListItem2=itemView.findViewById(R.id.layAdptOrderListItem2);
+            layAdptOrderListItem3=itemView.findViewById(R.id.layAdptOrderListItem3);
+            layAdptOrderListItem4=itemView.findViewById(R.id.layAdptOrderListItem4);
+            layAdptOrderListItem5=itemView.findViewById(R.id.layAdptOrderListItem5);
+            layAdptOrderListItem6=itemView.findViewById(R.id.layAdptOrderListItem6);
+            layAdptOrderListItem7=itemView.findViewById(R.id.layAdptOrderListItem7);
+            layAdptOrderListItem8=itemView.findViewById(R.id.layAdptOrderListItem8);
         }
     }
 }
